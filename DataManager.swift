e3c3e085428cd862a1328c4c4700aec14bb357b9
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-10-12 14:05:57 -0700

    first commit

---
+++ DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  Topcoder-FunSeries-SurveyApp
+//
+//  Created by Rodrigo Carballo on 10/11/15.
+//  Copyright © 2015 topcoder. All rights reserved.
+//
+
+import Foundation
---
+++ Topcoder-FunSeries-SurveyApp/AppModel.swift
@@ -0,0 +1,9 @@
+//
+//  AppModel.swift
+//  Topcoder-FunSeries-SurveyApp
+//
+//  Created by Rodrigo Carballo on 10/11/15.
+//  Copyright © 2015 topcoder. All rights reserved.
+//
+
+import Foundation
